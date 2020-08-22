555e10fd0cde3ddc9a937c5b1ba934788cc3b408
Bellonio <bellonegiulios@gmail.com>
2020-08-22 15:31:27 +0200

    First commit.

---
+++ app/src/main/java/com/bellone/instagram/MainActivity.java
@@ -0,0 +1,33 @@
+/*App that I made for myself because I often open Instagram but forgetting to switch on WiFi Network
+and so i consume a lot of Mobile Network 's MB*/
+
+package com.bellone.instagram;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.wifi.WifiManager;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    private WifiManager wifiManager = null;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        wifiManager = (WifiManager) getApplicationContext().getSystemService(WIFI_SERVICE);
+        if(wifiManager.getWifiState() != WifiManager.WIFI_STATE_ENABLED){
+            wifiManager.setWifiEnabled(true);
+        }
+
+        Intent intent = getPackageManager().getLaunchIntentForPackage("com.instagram.android");
+        startActivity(intent);
+    }
+}
